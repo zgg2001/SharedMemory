122e41594f629c4405c3823502c5f5029af58f4f
zgg2001 <397606244@qq.com>
2021-11-01 17:37:21 +0800

    守护进程 获取内存极值储存进共享内存

---
+++ src/Mem3/memcatch.cpp
@@ -0,0 +1,66 @@
+/*************************************************************************
+	> File Name: memcatch.cpp
+	> Author: ZHJ
+	> Remarks: 此文件执行任务
+	> Created Time: Sat 30 Oct 2021 07:56:07 PM CST
+ ************************************************************************/
+
+#include<iostream>
+#include<unistd.h>
+#include<sys/sysinfo.h>
+#include"semaphore.h"
+#include"shared_memory.h"
+
+using namespace std;
+
+int main()
+{
+    //获取程序运行时的内存消耗
+    struct sysinfo s_info;
+    //相关对象
+    semaphore *sem = new semaphore((key_t)0x1111);
+    shared_memory *mem = new shared_memory((key_t)0x2222);
+    int* list;
+    //初始化信号灯
+    sem->init();
+    //获取内存
+    mem->getmem(sizeof(int) * 2); //max 和 min
+    //此内存是否被初始化
+    bool init = false;
+
+    while(1)
+    {
+        //等待信号灯挂出
+        sem->wait();
+        //操作
+        list = (int*)mem->getptr();
+        
+        //获取当前消耗内存
+        int error = sysinfo(&s_info);
+        if(init == false && list[0] == 0 && list[1] == 0)
+        {
+            list[0] = s_info.freeram;
+            list[1] = s_info.freeram;
+            init = true;
+        }
+        else
+        {
+            if(list[0] < s_info.freeram)
+            {
+                list[0] = s_info.freeram;
+            }
+            else if(list[1] > s_info.freeram)
+            {
+                list[1] = s_info.freeram;
+            }
+        }
+        
+        //分离内存
+        mem->memdt((void*)list);
+        //挂出信号灯
+        sem->hangout();
+        sleep(60);
+    }
+
+    return 0;
+}
